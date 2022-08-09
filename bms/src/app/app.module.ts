143eab696f3d391fa9f4b4a0f1253fc52222b336
Uday Kumar Nemmoju (TMNA) <uday.kumar.nemmoju@toyota.com>
2022-08-09 21:21:01 +0530

    header components are created

--- bms/src/app/app.module.ts
+++ bms/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderOneComponent } from './header-one/header-one.component';
+import { HeaderTwoComponent } from './header-two/header-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderOneComponent,
+    HeaderTwoComponent
   ],
   imports: [
     BrowserModule,

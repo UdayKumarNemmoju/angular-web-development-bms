629f3e8af098ed25882d09c8d7e35a4c6026c105
Raj9562 <110969270+Raj9562@users.noreply.github.com>
2022-08-12 02:59:34 +0530

    updated movies component

--- bms/src/app/movies/movies.component.ts
+++ bms/src/app/movies/movies.component.ts
@@ -145,6 +145,18 @@ export class MoviesComponent implements OnInit {
   ]
 
 
+  header: string = "Enjoy Online Ticket Booking for Movies in Hyderabad With BookMyShow"
+  header1: string = "Latest Movies To Watch in Hyderabad With Family And Friends"
+  header2: string = "Upcoming Bollywood & Hollywood Movies That You Can't Miss "
+  header3: string = "Exciting Tollywood & Kollywood Movies To Book Tickets For "
+  header4: string = "The Joy Of Movie Tickets Bookings with Just a Few Clicks "
+  para: string = "If you are planning for movie ticket bookings for the latest movies in Hyderabad, don't look any further. Now it is easy to get on with online ticket booking with BookMyShow. Your one-stop solution for movies to watch this weekend.  Everyone enjoys watching their favorite movies on the big screen, and the excitement of watching it with friends is unparalleled. If you have been eagerly waiting for a movie that you can watch with your friends and family, now you know where to get the tickets from. When you watch a film in a cinema theatre, you get to watch it on a massive screen with surround-sound, and that enhances your movie-watching experience. Thus, allowing you to be a part of the actual  movie. Get to know about all movies and movie trailers to watch here. Also, know how to book movie tickets."
+  para1: string = "Each year the cinema world is enlightened with the latest movie trailers, increasing the excitement among everyone. This year, just like the previous year, you have been waiting for some of the biggest Bollywood movies to be released   with the biggest star cast. Enjoy your favourite movie, not just with your friends, but in a cinema hall Hyderabad that will be filled with like-minded people. Be a part of everyone's reaction Dates are already announced, and all you need to   do is book the tickets for the preferred date so that you dont end up missing the first-day first show! Don't worry we have the list of shows near you and movie showtimes."
+  para2: string = "Have you checked out the latest movie reviews of some of the best Hollywood movies? If so, we bet you would want to watch them all in the nearest movie theatre! With the anticipation of the release of Hit: The First Case, who would like to miss the first-day first show experience? The Hollywood movies running in cinemas now are already making the audience want for more, and with the new releases happening in the coming months, we recommend booking the tickets now in Hyderabad. Check out all the latest movie trailers here!"
+  para3: string = "Just like Bollywood and Hollywood movies, Tollywood seems to have a few good movie showtimes as well. You can plan for movies to watch this Friday with these Tollywood movies because the star cast is superb, and the storylines of these movies have already started making news. It will be super interesting to watch Taapsee Pannu.Keep an eye on the release date of Shabaash Mithu in Hyderabad. Don't miss any upcoming movies."
+  para4: string = "Grab on your popcorn because there are many movies to watch today in Hyderabad. If you want to save some money, dont miss out on our movie offers and discounts. Check out the movies running in cinemas time, and call all your friends to enjoy the best movie-watching experience together. There are many big releases in the pipeline, and it is expected that these movies will have the perfect casting and direction. Get ready for upcoming movies in theatres."
+  para5: string = "Dont wait anymore and book your movie tickets from BookMyShow today at the best price! Your access to your favourite movie in Hyderabad is only a click away! "
+
   ngOnInit(): void {
   }
 
